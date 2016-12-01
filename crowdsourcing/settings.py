7baac5d347a657765bbc58b6c15be81038133eee
Bingbing Huang <bing2013@cs.washington.edu>
2016-11-30 20:07:57 -0800

    Dependencies added

--- crowdsourcing/settings.py
+++ crowdsourcing/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 AUTH_USER_MODEL = 'crowdsourcing.CustomUser'
 AUTH_USER_MODEL_USERNAME_FIELD = 'name'
 LOGIN_REDIRECT_URL = 'home'
